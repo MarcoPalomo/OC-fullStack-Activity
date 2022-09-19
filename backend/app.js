b0cb068626dd6f516619b9b99ea8a547e441ebab
L. Marco P <marvi.pame.pro@gmail.com>
2022-09-20 00:49:06 +0200

    create product using post()

--- backend/app.js
+++ backend/app.js
@@ -23,45 +23,21 @@ product.use((req, res, next) => {
   next();
 });
 
-
-product.get('/api/products', (req, res, next) => {
+product.get('/api/products', (req, res) => {
     products.find({ _id: req.params.id })
       .then(() => res.status(201).json({ products: Product[0] }))
       .catch(error => res.status(400).json({ error }));
-    next();
 });
 
-product.get('/api/products/:id', (req, res, next) => {
+product.get('/api/products/:id', (req, res) => {
     product.findOne({ _id: req.params.id })
       .then(products => res.status(200).json(products))
       .catch(error => res.status(404).json({ error }));
-    next();
 });
 
-product.post('/api/products', (req, res, next) => {
+product.post('/api/products', (req, res) => {
   delete req.body._id;
-  res.status(201).json({ product: Product });
-  //const products = new Product({
-  //...req.body
-  next();
-});
-
-product.put('/api/products/:id', (req, res, next) => {
-  Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Modified!' }))
-    .catch(error => res.status(400).json({ error }));
-  next();
-});
-
-product.delete('/api/products/:id', (req, res, next) => {
-  product.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Deleted!' }))
-    .catch(error => res.status(400).json({ error }));
-  next();
-});
-
-product.use('/api/products', (req, res, next) => {
-    const products = [
+  const products = [
       {
         name: 'cuillere',
         description: 'Les infos de mon premier objet',
@@ -69,8 +45,21 @@ product.use('/api/products', (req, res, next) => {
         inStock: true,
       },
     ];
-    res.status(200).json(product);
-    next();
+  res.status(201).json({ product: Product });
+  //const products = new Product({
+  //...req.body
+});
+
+product.put('/api/products/:id', (req, res) => {
+  Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Modified!' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+product.delete('/api/products/:id', (req, res) => {
+  product.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Deleted!' }))
+    .catch(error => res.status(400).json({ error }));
 });
 
 product.use((req, res) => {
